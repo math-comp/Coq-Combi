c1789f30f21bda3d6c97762e3951fca5d2c823c0
Florent Hivert <Florent.Hivert@u-psud.fr>
2014-06-18 11:56:00 +0200

    Rewrote definition of Dyck by prefix

--- DyckPrefix.v
+++ DyckPrefix.v
@@ -7,14 +7,16 @@ Require Import Permutation.
 Require Import Coq.Sorting.PermutSetoid.
 Require Import PermutEq.
 
-Section Prefix.
+Section DyckPrefix.
 
 Notation list_contents := (list_contents _ eq_brace_dec).
 
-Definition mult (l : list Brace) (b : Brace) := multiplicity (list_contents l) b.
+Definition mult (w : list Brace) (b : Brace) := multiplicity (list_contents w) b.
+
+Definition mult_prefix (l : nat) (w : list Brace) :=  mult (prefix Brace l w).
 
 Ltac omega_brace :=
-  unfold mult in *;
+  unfold mult_prefix, mult in *;
   simpl in *;
   try rewrite ?if_close_close in *;
   try rewrite ?if_close_open  in *;
@@ -23,14 +25,14 @@ Ltac omega_brace :=
   omega.
 
 Definition dyck_prefix_height (h : nat) (w : list Brace) : Prop :=
-  (forall l : nat, (mult (prefix _ l w) Open) + h >= (mult (prefix _ l w) Close))
+  (forall l : nat, (mult_prefix l w Open) + h >= (mult_prefix l w Close))
   /\
   (mult w Open) + h = mult w Close.
 
 Lemma dyck_prefix_nil :
   forall h : nat, dyck_prefix_height h nil <-> dyck_height h nil.
 Proof.
-  unfold dyck_prefix_height, mult.
+  unfold dyck_prefix_height, mult_prefix, mult.
   destruct h; simpl.
   split.
   auto with arith.
@@ -68,6 +70,7 @@ Proof.
   destruct h.
   clear IHw H1.
   specialize H0 with 1.
+  unfold mult_prefix, mult in H0.
   simpl in H0.
   rewrite prefix_0 in H0.
   omega_brace.
@@ -78,6 +81,7 @@ Proof.
 
   intro l.
   specialize H0 with (S l).
+  unfold mult_prefix in H0.
   simpl in H0.
   omega_brace.
 
@@ -131,15 +135,38 @@ Proof.
   omega_brace.
 Qed.
 
-Definition dyck_prefix (w : list Brace) : Prop := dyck_prefix_height 0 w.
+
+Definition dyck_prefix (w : list Brace) : Prop :=
+  (forall l : nat, (mult_prefix l w Open) >= (mult_prefix l w Close))
+  /\
+  mult w Open = mult w Close.
+
+(* Definition dyck_prefix (w : list Brace) : Prop := dyck_prefix_height 0 w. *)
 
 Theorem prefix_dyck_equiv_dyck :
   forall w : list Brace, is_dyck w <-> dyck_prefix w.
 Proof.
+  unfold dyck_prefix.
   split.
-  apply prefix_impl_dyck.
-  apply dyck_impl_prefix.
+  intro H.
+  specialize prefix_impl_dyck with w 0; intros Hd.
+  unfold dyck_prefix_height in Hd.
+  elim Hd; clear Hd; auto.
+  intros H1 H2.
+  split.
+  intro l; specialize H1 with l.
+  rewrite <- plus_n_O in H1; assumption.
+  rewrite <- plus_n_O in H2; assumption.
+  specialize dyck_impl_prefix with w 0; intro Hd.
+  unfold dyck_prefix_height in Hd.
+  intros.
+  apply Hd; clear Hd.
+  decompose [and] H; clear H.
+  split.
+  intros l.
+  rewrite <- plus_n_O; apply H0.
+  rewrite <- plus_n_O; assumption.
 Qed.
 
 
-End Prefix.
+End DyckPrefix.
