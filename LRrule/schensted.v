d579de5cd73b8ca7420088bd6bee98dbf60f71a2
Florent Hivert <Florent.Hivert@u-psud.fr>
2014-08-20 13:21:26 +0200

    Work on Bijection Tab, Yam -> w / rewrote is_part def

--- LRrule/schensted.v
+++ LRrule/schensted.v
@@ -4,6 +4,10 @@ Require Import subseq.
 Set Implicit Arguments.
 Unset Strict Implicit.
 
+Lemma ieqi1F i : (i == i.+1) = false.
+Proof. by elim: i => [//=| I IHi]; rewrite eqSS. Qed.
+
+
 Section Rows.
 
   Implicit Type l : nat.
@@ -837,27 +841,51 @@ Section Shape.
   Implicit Type t : seq (seq nat).
 
   Definition shape t := map size t.
-  Definition is_part sh :=
-    (last 1 sh != 0) &&
-    all (fun i => (nth 0 sh i) >= nth 0 sh i.+1) (In (size sh)).
+  Fixpoint is_part sh :=
+    if sh is sh0 :: sh'
+    then (sh0 >= head 1 sh') && (is_part sh')
+    else true.
   Definition is_out_corner sh i := nth 0 sh i > nth 0 sh i.+1.
+
+  Lemma is_part_tl l0 sh : is_part (l0 :: sh) -> is_part sh.
+  Proof. by move => /= /andP []. Qed.
 
   Lemma is_partP sh :
     reflect (last 1 sh != 0 /\ forall i, (nth 0 sh i) >= nth 0 sh i.+1) (is_part sh).
   Proof.
-    rewrite /is_part; apply (iffP idP).
-    - move=> /andP [] Hn0 /allP => Hall; split; first exact Hn0.
-      move {Hn0} => i; case (ltnP i (size sh)) => Hi.
-      * apply Hall; by rewrite mem_iota add0n.
-      * rewrite (nth_default _ Hi) nth_default; first by [].
-        by apply (leq_trans Hi).
-    - move=> [] -> Hi; apply /allP => i _; by apply Hi.
+    apply (iffP idP).
+    - elim: sh => [ //= | sh0 sh IHsh] /= /andP [] Hhead Hpart.
+      move: (IHsh Hpart) => {IHsh} [] Hlast Hi.
+      split; first by case: sh Hhead Hpart Hlast Hi => [/= | sh1 sh //=]; case sh0.
+      case => [|i] /=; first by move: Hhead; case sh.
+      exact (Hi i).
+    - elim: sh => [ //= | sh0 sh IHsh] /= [] Hlast Hpart.
+      apply/andP; split.
+      * move: (Hpart 0) Hlast => /=; case sh => //=.
+        by rewrite /head ltn_neqAle eq_sym => -> ->.
+      * apply IHsh; split; last by move=> i; apply (Hpart i.+1).
+        move: Hlast; by case sh.
   Qed.
 
-  Lemma stupid i : (i == i.+1) = false.
-  Proof. by elim: i => [//=| I IHi]; rewrite eqSS. Qed.
+  Lemma part_head0F sh : head 1 sh == 0 -> is_part sh = false.
+  Proof.
+    elim: sh => [//= | sh0 sh IHsh] /= /eqP ->.
+    rewrite leqn0; case (boolP (head 1 sh == 0)) => //=.
+  Qed.
 
-  Lemma del_cornerE sh i :
+  Lemma part_head_non0 sh : is_part sh -> head 1 sh != 0.
+  Proof.
+    elim: sh => [//= | sh0 sh IHsh] /= /andP [] Head.
+    move/IHsh; apply contraLR; rewrite !negbK => /eqP Hsh0.
+    by move: Head; rewrite Hsh0 leqn0.
+  Qed.
+
+  (* unused lemma *)
+  Lemma part0F s : is_part (0 :: s) = false.
+  Proof. case (boolP (is_part (0 :: s))) => [|//=]. by move/part_head_non0. Qed.
+
+  (* unused lemma *)
+  Lemma del_corner sh i :
     last 1 sh != 0 -> is_part (incr_nth sh i) ->
     is_out_corner (incr_nth sh i) i = is_part sh.
   Proof.
@@ -865,10 +893,10 @@ Section Shape.
     apply (sameP (@idP (is_out_corner _ i))); apply (equivP (@idP (is_part _))).
     rewrite /is_out_corner; split.
     - move=> /is_partP => [] [] _ Hpart; move: (Hpart i) => {Hpart}.
-      by rewrite !nth_incr_nth stupid eq_refl add0n add1n ltnS.
+      by rewrite !nth_incr_nth ieqi1F eq_refl add0n add1n ltnS.
     - move=> Hcorn; apply /is_partP; split; first exact Hn0.
       move=> j; move: Hcorn (Hpart1 j).
-      rewrite !nth_incr_nth stupid eq_refl add0n add1n ltnS => Hcorn.
+      rewrite !nth_incr_nth ieqi1F eq_refl add0n add1n ltnS => Hcorn.
       case (altP (i =P j)) => [<- //=|_].
       case (altP (i =P j.+1)) => [Hi |_].
       * rewrite add0n add1n; apply ltnW.
@@ -877,15 +905,9 @@ Section Shape.
 
   Lemma is_part_sht t : is_tableau t -> is_part (shape t).
   Proof.
-    rewrite /shape => Ht; apply /is_partP.
-    split.
-    - elim: t Ht => [//= | t0 t IHt] /= /and4P [] Ht0 _ _ Htab.
-      move: (map size _) (IHt Htab) => s.
-      case: s => //= _; by case: t0 Ht0.
-    - elim: t Ht => [_ i |t0 t IHt /= /and4P [] Hnnil Hrow /dominateP [] Hdom _ Htab i];
-        first by rewrite nth_nil.
-      case: i => [|i] /=; first by move: Hdom; case t.
-      by apply IHt.
+    elim: t => [//= | t0 t IHt] /= /and4P [] Ht0 _ /dominateP [] Hdom _ Htab.
+    rewrite (IHt Htab) andbT {IHt Htab} /shape.
+    case: t Hdom => //=; by case: t0 Ht0.
   Qed.
 
   Fixpoint shape_rowseq s :=
@@ -920,6 +942,9 @@ Section Shape.
         - by rewrite (IHs (leq_trans Hs Hi)) (ltn_eqF Hi).
   Qed.
 
+  Lemma shape_rowshape_cons l s : shape_rowseq (l :: s) = incr_nth (shape_rowseq s) l.
+  Proof. by []. Qed.
+
   Fixpoint is_yam s :=
     if s is s0 :: s'
     then is_part (shape_rowseq s) && is_yam s'
@@ -931,11 +956,36 @@ Section Shape.
   Lemma is_yam_tl l0 s : is_yam (l0 :: s) -> is_yam s.
   Proof. by move=> /= /andP []. Qed.
 
+  Fixpoint shift_yam s :=
+    if s is s0 :: s'
+    then if s0 is s0'.+1
+         then s0' :: (shift_yam s')
+         else (shift_yam s')
+    else [::].
+
+  Lemma shape_shift s : shape_rowseq (shift_yam s) = behead (shape_rowseq s).
+  Proof.
+    elim: s => [//= | s0 s /= IHs].
+    case s0 => [ | s0' /=].
+    - rewrite IHs /=; by case (shape_rowseq s).
+    - rewrite IHs /=; case (shape_rowseq s) => [|sh0 sh //=].
+      by case: s0'.
+  Qed.
+
+  Lemma is_yam_shift s : is_yam s -> (is_yam (shift_yam s)).
+  Proof.
+    elim: s => [//= | s0 s IHs] /= /andP [] Hpart.
+    move/IHs {IHs} => Hyam; case: s0 Hpart=> [//= | s0'] /=.
+    rewrite Hyam andbT shape_shift.
+    move H : (shape_rowseq s) => [| sh0 sh] /= /andP [] _ //=.
+    by case s0'.
+  Qed.
+
   Lemma is_out_corner_yam l0 s :
     is_yam (l0 :: s) -> is_out_corner (shape_rowseq (l0 :: s)) l0.
   Proof.
     move=> Hyam; move: (is_part_shyam (is_yam_tl Hyam)) => /is_partP [] _ Hpart.
-    rewrite /is_out_corner !nth_incr_nth stupid eq_refl add0n add1n ltnS.
+    rewrite /is_out_corner !nth_incr_nth ieqi1F eq_refl add0n add1n ltnS.
     by apply Hpart.
   Qed.
 
@@ -1145,10 +1195,24 @@ Section Inverse.
   Qed.
 
   Lemma shape_instabninv1 t row srow :
-    is_tableau t -> shape t == shape_rowseq (row :: srow) ->
+    is_yam (row :: srow) -> shape t == shape_rowseq (row :: srow) ->
     shape (invinstabn t row).1 == shape_rowseq srow.
   Proof.
-    admit.
+    elim: row srow t => [|row IHrow] srow t Hyam.
+    - case: t => [//= | r0 t]; first by case (shape_rowseq srow).
+      rewrite shape_rowshape_cons => Hshape.
+      move: (is_part_shyam (is_yam_tl Hyam)) => Hpart {Hyam}.
+      case: r0 Hpart Hshape => [/= | l0 [| l1 r0'] ] Hpart Hshape /=.
+      * by case: (shape_rowseq srow) Hshape.
+      * case: (shape_rowseq srow) Hpart Hshape => //=.
+        case => [|//=] l /andP []; rewrite leqn0.
+        by move/part_head0F => ->.
+      * move: Hshape; rewrite size_belast /=.
+        by case: (shape_rowseq srow).
+    - case: t => [//= | r0 t]; first by case (shape_rowseq srow).
+      rewrite shape_rowshape_cons => Hshape.
+      move => /=.
+      admit.
   Qed.
 
   Theorem RS_bij_2 pair : RSpair pair -> RSbij (RSbijinv2 pair) == pair.
@@ -1163,7 +1227,7 @@ Section Inverse.
       by case (ltnP row n) => //= /ltn_predK <-.
     move: (instabninvK Htab Hnnil Hcorn).
     move: (is_tableau_instabninv1 Htab Hcorn).
-    move: (shape_instabninv1 Htab Hshape).
+    move: (shape_instabninv1 Hyam Hshape).
     move Hinvins: (invinstabn tab row) => [tin l] /= Hshapetin Htabtin.
     rewrite rev_rcons /=.
     move: Hyam => /= /andP [] Hincr Hyam.
