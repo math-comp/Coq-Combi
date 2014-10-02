95a8232021f582c7f87aef8b8790445b34f4c706
Florent Hivert <Florent.Hivert@u-psud.fr>
2014-10-02 18:43:38 +0200

    Standard tableaux

---
+++ LRrule/stdtab.v
@@ -0,0 +1,362 @@
+(******************************************************************************)
+(*       Copyright (C) 2014 Florent Hivert <florent.hivert@lri.fr>            *)
+(*                                                                            *)
+(*  Distributed under the terms of the GNU General Public License (GPL)       *)
+(*                                                                            *)
+(*    This code is distributed in the hope that it will be useful,            *)
+(*    but WITHOUT ANY WARRANTY; without even the implied warranty of          *)
+(*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       *)
+(*    General Public License for more details.                                *)
+(*                                                                            *)
+(*  The full text of the GPL is available at:                                 *)
+(*                                                                            *)
+(*                  http://www.gnu.org/licenses/                              *)
+(******************************************************************************)
+Require Import ssreflect ssrbool ssrfun ssrnat eqtype finfun fintype choice seq tuple.
+Require Import finset perm tuple.
+Require Import subseq partition permuted ordtype schensted plactic greeninv.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+(******************************************************************************)
+(* Bijection between Yamanouchi word and standard tableau                     *)
+(* Main results:                                                              *)
+(* Bijection:
+
+Lemma   stdtab_of_yamP y : is_yam y    -> is_stdtab (stdtab_of_yam y).
+Theorem stdtab_of_yamK y : is_yam y    -> yam_of_stdtab (stdtab_of_yam y) = y.
+Lemma   yam_of_stdtabP t : is_stdtab t -> is_yam (yam_of_stdtab t).
+Theorem yam_of_stdtabK t : is_stdtab t -> stdtab_of_yam (yam_of_stdtab t) = t.
+
+Statistic:
+
+Lemma shape_stdtab_of_yam y : shape (stdtab_of_yam y) = shape_rowseq y.
+Lemma shape_yam_of_stdtab t : is_stdtab t -> shape_rowseq (yam_of_stdtab t) = shape t.
+Lemma size_stdtab_of_yam y  : size_tab (stdtab_of_yam y) = size y.
+Lemma size_yam_of_stdtab t  : is_stdtab t -> size (yam_of_stdtab t) = size_tab t.
+                                                                              *)
+(******************************************************************************)
+
+
+Section Bijection.
+
+Implicit Type y : seq nat.
+Implicit Type t : seq (seq nat).
+
+Definition is_stdtab t := is_tableau t && perm_eq (to_word t) (iota 0 (size_tab t)).
+
+Fixpoint stdtab_of_yam y :=
+  if y is y0 :: y' then
+    append_nth (stdtab_of_yam y') (size y') y0
+  else [::].
+
+Lemma shape_stdtab_of_yam y : shape (stdtab_of_yam y) = shape_rowseq y.
+Proof. elim: y => [//= | y0 y IHy] /=. by rewrite shape_append_nth IHy. Qed.
+
+Lemma size_stdtab_of_yam y : size_tab (stdtab_of_yam y) = size y.
+Proof.
+  elim: y => [//= | y0 y IHy] /=.
+  by rewrite -IHy {IHy} /size_tab shape_append_nth sum_incr_nth.
+Qed.
+
+Lemma perm_eq_append_nth t x pos :
+  perm_eq (to_word (append_nth t x pos)) (x :: to_word t).
+Proof.
+  rewrite /append_nth /to_word; elim: t pos => [//= | t0 t IHt] /=.
+  + elim => [//= | pos IHpos].
+    move: IHpos; rewrite /= rev_cons flatten_rcons cats0.
+    by rewrite nth_default //= set_nth_nil.
+  + case => [//= | pos].
+    * rewrite !rev_cons !flatten_rcons -cats1 -[x :: (_ ++ _)]cat1s.
+      rewrite [flatten _ ++ _ ++ _]catA.
+      apply perm_eqlE; by apply perm_catC.
+    * have {IHt} IHt := IHt pos.
+      rewrite !rev_cons !flatten_rcons.
+      move: IHt; set T := (flatten _).
+      by rewrite -[x :: flatten (rev t) ++ t0]/((x :: flatten (rev t)) ++ t0) perm_cat2r.
+Qed.
+
+Lemma perm_eq_stdtab_of_yam y : perm_eq (to_word (stdtab_of_yam y)) (iota 0 (size y)).
+Proof.
+  elim: y => [//= | y0 y IHy].
+  have -> /= : iota 0 (size (y0 :: y)) = rcons (iota 0 (size y)) (size y).
+    rewrite -[size (y0 :: y)]/((size y).+1) -addn1 iota_add add0n /=.
+    by apply cats1.
+  apply (perm_eq_trans (perm_eq_append_nth _ _ _)).
+  move: IHy; rewrite -(perm_cons (size y)) => /perm_eq_trans; apply.
+  rewrite perm_eq_sym; apply/perm_eqlP; by apply perm_rcons.
+Qed.
+
+Lemma is_tab_append_nth_size r T n :
+   all (gtn n) (to_word T) ->
+   is_part (incr_nth (shape T) r) ->
+   is_tableau T -> is_tableau (append_nth T n r).
+Proof.
+  elim: T r => [//= | T0 T IHT] r /=; first by case: r => [//= | r] /=; elim: r.
+  rewrite /to_word rev_cons flatten_rcons all_cat -[flatten (rev T)]/(to_word T).
+  move=> /andP [] Hall Hall0.
+  case: r => [/= _ | r] /=.
+  - move=> {IHT Hall} /and4P [] Hnnil Hrow Hdom ->.
+    rewrite (dominate_rcons _ Hdom) {Hdom} !andbT.
+    apply /andP; split; first by case T0.
+    apply (is_row_rcons Hrow); rewrite {Hrow} leqXnatE.
+    case/lastP: T0 Hnnil Hall0 => [//= | T0 T0n] _.
+    rewrite all_rcons last_rcons /= andbC => /andP [] _.
+    by apply ltnW.
+  - move => /andP [] Hhead Hpart /and4P [] -> -> Hdom Htab /=.
+    rewrite IHT //= andbT {IHT Hall Hpart Htab}.
+    case: r Hhead => [| r]; last by case: T Hdom => [//= | T1 T].
+    case: T Hdom => [_ | T1 T] /=.
+    + case: T0 Hall0 => [//= | hT0 T0] /= /andP [] Hn _ _.
+      apply/dominateP; split=> //=; case => [|//=] _ /=.
+      by rewrite ltnXnatE.
+    + move/dominateP => [] _ Hdom Hsize.
+      apply/dominateP; split; rewrite size_rcons; first by [].
+      move=> i Hi; rewrite nth_rcons; case (ltnP i (size T1)) => Hi1.
+      * by apply Hdom.
+      * have -> : i == size T1 by rewrite eqn_leq Hi1 andbT -ltnS.
+        move: Hall0 => /allP Hall0.
+        have /Hall0 /= := (mem_nth (inhabitant nat_ordType) (leq_trans Hi Hsize)).
+        by rewrite ltnXnatE.
+Qed.
+
+Lemma stdtab_of_yamP y : is_yam y -> is_stdtab (stdtab_of_yam y).
+Proof.
+  rewrite /is_stdtab.
+  rewrite /size_tab shape_stdtab_of_yam shape_rowseq_eq_size perm_eq_stdtab_of_yam andbT.
+  elim: y => [//= | y0 y IHy] /= /andP [] Hpart /IHy {IHy}.
+  move: Hpart; rewrite -shape_stdtab_of_yam.
+  suff : all (gtn (size y)) (to_word (stdtab_of_yam y)) by apply is_tab_append_nth_size.
+  have /perm_eq_mem := perm_eq_stdtab_of_yam y.
+  move: (to_word (stdtab_of_yam y)) => w Hperm.
+  apply /allP => x Hx /=.
+  have:= Hperm x; by rewrite mem_iota /= add0n Hx => /esym ->.
+Qed.
+
+Lemma perm_eq_rembig (Alph : ordType) (u v : seq Alph) :
+  perm_eq u v -> perm_eq (rembig u) (rembig v).
+Proof.
+  case Hu: u => [/= | u0 u']; case Hv: v => [//= | v0 v'].
+  + by rewrite perm_eq_nilF.
+  + by rewrite perm_eq_sym perm_eq_nilF.
+  move=> Hperm; have Hmax:= maxL_perm_eq Hperm; move: Hmax Hperm.
+
+  have:= eq_refl (rembig u); rewrite {2}Hu => /rembigP Htmp.
+  have /Htmp {Htmp} : u0 :: u != [::] by [].
+  move=> [] u1 [] bu [] u2 []; rewrite {1}Hu => -> Hub Hlequ Hltnu.
+  have {Hlequ Hltnu} -> := maxL_LbR Hub Hlequ (maxLtnW Hltnu).
+  rewrite Hub {u Hu Hub u0 u'}.
+
+  have:= eq_refl (rembig v); rewrite {2}Hv => /rembigP Htmp.
+  have /Htmp {Htmp} : v0 :: v != [::] by [].
+  move=> [] v1 [] bv [] v2 []; rewrite {1}Hv => -> Hvb Hleqv Hltnv.
+  have {Hleqv Hltnv} -> := maxL_LbR Hvb Hleqv (maxLtnW Hltnv).
+  rewrite Hvb {v Hv Hvb v0 v'}.
+
+  rename bv into mx; move ->.
+  rewrite -[mx :: u2]cat1s -[mx :: v2]cat1s -[perm_eq (u1 ++ u2) _](perm_cons mx).
+  have Hlemma u v : perm_eq (u ++ [:: mx] ++ v) (mx :: u ++ v).
+    rewrite catA -[mx :: u ++ v]/((mx :: u) ++ v) perm_cat2r -[mx :: u]cat1s.
+    apply perm_eqlE; by apply perm_catC.
+  move=> H; have:= Hlemma u1 u2; rewrite perm_eq_sym.
+  move/perm_eq_trans; apply.
+  apply (perm_eq_trans H).
+  by apply Hlemma.
+Qed.
+
+Lemma maxL_iota n i : foldl (maxX (T:=nat_ordType)) i (iota i.+1 n) = i + n.
+Proof. elim: n i => [//= | n IHn] /= i. by rewrite {2}/maxX ltnXnatE ltnSn IHn addSnnS. Qed.
+
+Lemma rembig_iota n i : rembig (iota i n.+1) = iota i n.
+Proof.
+  elim: n i => [//= | n IHn] i.
+  have /= -> := (IHn i.+1).
+  by rewrite maxL_iota ltnXnatE ltnNge addSnnS leq_addr.
+Qed.
+
+
+Fixpoint yam_of_stdtab_rec n t :=
+  if n is n'.+1 then
+    (last_big t n') :: yam_of_stdtab_rec n' (RS (rembig (to_word t)))
+  else [::].
+Definition yam_of_stdtab t := yam_of_stdtab_rec (size_tab t) t.
+
+Lemma size_yam_of_stdtab_rec n t : size (yam_of_stdtab_rec n t) = n.
+Proof. elim: n t => [//= | n IHn] /= t; by rewrite IHn. Qed.
+
+
+Theorem yam_of_stdtabK t : is_stdtab t -> stdtab_of_yam (yam_of_stdtab t) = t.
+Proof.
+  rewrite /yam_of_stdtab /is_stdtab => /andP [].
+  move H : (size_tab t) => n.
+  elim: n t H => [//= | n IHn] t Hsize Htab Hperm.
+    move: Hsize => /tab0 H; by rewrite (H Htab).
+  move Hw : (to_word t) Hsize => w; case : w Hw => [//= | w0 w].
+    by rewrite size_to_word => -> /=.
+  move=> Hw Hsize.
+  have := RS_tabE Htab; rewrite Hw => Ht; have:= Ht.
+  have:= (rembig_RS_last_big w0 w) => -> {2}<- /=.
+  set t' := RS (rembig (w0 :: w)).
+  have Htab' := is_tableau_RS (rembig (w0 :: w)).
+  have Hsize' : size_tab t' = n.
+    by rewrite (eqP (size_RS _)) size_rembig -Hw -size_to_word Hsize.
+  have Hperm' : perm_eq (to_word t') (iota 0 n).
+    rewrite -Hsize'; move: Hperm.
+    rewrite /t' (eqP (size_RS _)) size_rembig -Hw -size_to_word Hsize /= => Hperm.
+    have:= plactcongr_homog (congr_RS (rembig (to_word t))).
+    rewrite perm_eq_sym => /perm_eq_trans; apply.
+    move: Hperm; rewrite Hw.
+    move/perm_eq_rembig/perm_eq_trans; apply.
+    rewrite rembig_iota; by apply perm_eq_refl.
+  have:= IHn t' Hsize' Htab' Hperm'.
+  rewrite size_yam_of_stdtab_rec Ht Hw => ->.
+  suff -> : (foldl (maxX (T:=nat_ordType)) w0 w) = n by [].
+  move: Hperm; rewrite Hw /= => /maxL_perm_eq ->.
+  by rewrite maxL_iota.
+Qed.
+
+
+Lemma find_append_nth l t r :
+  l \notin (to_word t) -> find (fun x => l \in x) (append_nth t l r) = r.
+Proof.
+  rewrite /append_nth.
+  elim: r t => [/= | r IHr] t /=; case: t => [_ | t0 t] /=.
+  + by rewrite inE eq_refl.
+  + by rewrite mem_rcons inE eq_refl /=.
+  + apply /eqP; rewrite eqSS.
+    elim: r {IHr} => [//= | r]; first by rewrite inE eq_refl.
+    by rewrite /ncons /= => /eqP ->.
+  + rewrite /to_word rev_cons flatten_rcons mem_cat negb_or.
+    by rewrite -[flatten (rev t)]/(to_word t) => /andP [] /IHr {IHr} -> /negbTE ->.
+Qed.
+
+Lemma size_notin_stdtab_of_yam y : (size y) \notin (to_word (stdtab_of_yam y)).
+Proof.
+  have /perm_eq_mem -> := perm_eq_stdtab_of_yam y.
+  by rewrite mem_iota add0n /= ltnn.
+Qed.
+
+Lemma rconsK (T : eqType) (l : T) u v : rcons u l = rcons v l -> u = v.
+Proof.
+  elim: u v => [| u0 u IHu]; case=> [|v0 v] //= [].
+  + move=> _; by case v.
+  + move=> _; by case u.
+  + by move=> -> /IHu ->.
+Qed.
+
+Lemma incr_nth_injl u v i :
+  0 \notin u -> 0 \notin v -> incr_nth u i = incr_nth v i -> u = v.
+Proof.
+  move=> Hu Hv /eqP; elim: u v Hu Hv i => [/= v _ | u0 u IHu v] /=; case: v => [//= | v0 v].
+  + rewrite inE negb_or => /andP [] Hv0 _.
+    by case=> [| i] /= /eqP [] H; rewrite H eq_refl in Hv0.
+  + move=> {IHu}.
+    rewrite inE negb_or => /andP [] Hu0 _ _.
+    by case=> [| i] /= /eqP [] H; rewrite H eq_refl in Hu0.
+  rewrite !inE !negb_or => /andP [] Hu0 Hu /andP [] Hv0 Hv.
+  case=> [| i] /= /eqP [] ->; first by move ->.
+  by move/eqP/(IHu _ Hu Hv) ->.
+Qed.
+
+Lemma shape0 (T : ordType) (u : seq (seq T)) : [::] \notin u -> 0 \notin (shape u).
+Proof.
+  apply contra; rewrite /shape; elim: u => [//=| u0 u IHu] /=.
+  rewrite !inE => /orP [].
+  + by rewrite eq_sym => /nilP ->.
+  + move/IHu ->; by rewrite orbT.
+Qed.
+
+Lemma append_nth_injl (T : ordType) u v (l : T) p :
+  [::] \notin u -> [::] \notin v -> append_nth u l p = append_nth v l p -> u = v.
+Proof.
+  move=> Hu Hv Heq; have:= erefl (shape (append_nth u l p)); rewrite {2}Heq.
+  rewrite !shape_append_nth => /(incr_nth_injl (shape0 Hu) (shape0 Hv)) => /eqP {Hu Hv}.
+  move: Heq; rewrite /append_nth.
+  elim: u v p => [/= | u0 u IHu] /=; case=> [| v0 v] //=.
+  case=> [| p] /= []; first by move/rconsK => -> ->.
+  by move=> -> Heq /eqP [] /eqP /(IHu _ _ Heq) ->.
+Qed.
+
+Lemma stdtab_of_yam_nil y : is_yam y -> [::] \notin (stdtab_of_yam y).
+Proof.
+  move/stdtab_of_yamP; rewrite /is_stdtab => /andP [] Htab _.
+  elim: (stdtab_of_yam) Htab => [//= | t0 t IHt] /= /and4P [] Hnnil _ _ /IHt H.
+  by rewrite inE negb_or eq_sym Hnnil H.
+Qed.
+
+Lemma stdtab_of_yam_inj x y :
+  is_yam x -> is_yam y -> stdtab_of_yam x = stdtab_of_yam y -> x = y.
+Proof.
+  move=> Hx Hy H.
+  have:= eq_refl (size_tab (stdtab_of_yam x)); rewrite {2}H !size_stdtab_of_yam => Hsz.
+  elim: x y Hsz H Hx Hy => [| x0 x IHx]; case=> [//= | y0 y] //=.
+  rewrite eqSS => Heqsz; have:= Heqsz => /eqP -> Heq.
+  have Hfind w := find_append_nth _ (size_notin_stdtab_of_yam w).
+  have:= Hfind x0 x; rewrite (eqP Heqsz) Heq Hfind {Hfind} => H0.
+  move=> /andP [] _ Hx /andP [] _ Hy.
+  rewrite H0; congr (_ :: _); apply (IHx _ Heqsz) => {IHx Heqsz} //=.
+  move: Heq; rewrite H0 {H0}.
+  apply append_nth_injl; by apply stdtab_of_yam_nil.
+Qed.
+
+Lemma shape_yam_of_stdtab t : is_stdtab t -> shape_rowseq (yam_of_stdtab t) = shape t.
+Proof.
+  move/yam_of_stdtabK => H.
+  have:= erefl (shape t); by rewrite -{1}H shape_stdtab_of_yam.
+Qed.
+
+Lemma size_yam_of_stdtab t : is_stdtab t -> size (yam_of_stdtab t) = size_tab t.
+Proof.
+  move=> Htab.
+  by rewrite /size_tab -(shape_yam_of_stdtab Htab) shape_rowseq_eq_size.
+Qed.
+
+Lemma part_yam_of_stdtab t : is_stdtab t -> is_part (shape_rowseq (yam_of_stdtab t)).
+Proof.
+  move=> Htab; rewrite (shape_yam_of_stdtab Htab).
+  apply is_part_sht.
+  by move: Htab; rewrite /is_stdtab => /andP [].
+Qed.
+
+Lemma stdtab_rembig t : is_stdtab t -> is_stdtab (RS (rembig (to_word t))).
+Proof.
+  rewrite /is_stdtab => /andP [].
+  move H : (size_tab t) => n.
+  elim: n t H => [//= | n IHn] t Hsize Htab Hperm.
+  + by rewrite (tab0 Htab Hsize) /RS /=.
+  + rewrite is_tableau_RS /=.
+    apply (@perm_eq_trans _ (rembig (to_word t))).
+    - rewrite perm_eq_sym; apply plactcongr_homog; by apply congr_RS.
+    - apply (perm_eq_trans (perm_eq_rembig Hperm)).
+      rewrite rembig_iota.
+      by rewrite (eqP (size_RS _)) size_rembig -size_to_word Hsize.
+Qed.
+
+Lemma yam_of_stdtabP t : is_stdtab t -> is_yam (yam_of_stdtab t).
+Proof.
+  move=> Htab; have := part_yam_of_stdtab Htab.
+  rewrite /yam_of_stdtab.
+  move H : (size_tab t) => n.
+  elim: n t H Htab => [//= | n IHn] t Hsize Htab.
+  move Hw : (to_word t) Hsize => w; case : w Hw => [//= | w0 w].
+    by rewrite size_to_word => -> /=.
+  rewrite /= => Ht Hsize -> /=.
+  have Hsize' : size_tab (RS (rembig (to_word t))) = n.
+    by rewrite (eqP (size_RS _)) size_rembig -size_to_word Hsize.
+  apply (IHn _ Hsize').
+  + by apply stdtab_rembig.
+  + rewrite -Hsize' (shape_yam_of_stdtab (stdtab_rembig Htab)).
+    apply is_part_sht; by apply is_tableau_RS.
+Qed.
+
+Theorem stdtab_of_yamK y : is_yam y -> yam_of_stdtab (stdtab_of_yam y) = y.
+Proof.
+  move=> Hyam.
+  have /yam_of_stdtabK := (stdtab_of_yamP Hyam).
+  apply stdtab_of_yam_inj; last exact Hyam.
+  apply yam_of_stdtabP; by apply stdtab_of_yamP.
+Qed.
+
+End Bijection.
