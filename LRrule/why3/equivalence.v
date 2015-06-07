225894b09a79cdaea59a347c54e5c1d41ef7c8a0
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@lri.fr>
2015-06-07 12:51:37 +0200

    Coq files for the proof of equivalence

---
+++ LRrule/why3/equivalence.v
@@ -0,0 +1,16 @@
+
+(*
+   here we prove the equivalence between definitions from
+   - the Coq proof (in directory ..), and
+   - the Why3 proof (in file lrrule.mlw)
+*)
+
+Add LoadPath "..".
+Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq fintype.
+Require Import tuple finfun finset bigop path.
+Require Import tools partition yama ordtype tableau std stdtab.
+
+(* import definitions from Why3 *)
+Require Import Spec.
+
+
