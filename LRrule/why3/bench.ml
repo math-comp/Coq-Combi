211b0f1e47964a89e183c2ed060936f74d14bab4
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@lri.fr>
2015-07-09 15:26:34 +0200

    benchmarking the extracted code (wip)

---
+++ LRrule/why3/bench.ml
@@ -0,0 +1,34 @@
+
+open Format
+
+let () = Random.self_init ()
+
+let random_partition n =
+  let rec build acc m n = (* partition of n in parts at most m *)
+    if n = 0 then List.rev acc else
+      let m = min m n in
+      let p = 1 + Random.int m in
+      build (p :: acc) p (n - p) in
+  Array.of_list (build [] n n)
+
+let rows = int_of_string Sys.argv.(1)
+
+let inner = Array.init rows (fun _ -> Random.int 3)
+let () = for i = rows - 2 downto 0 do inner.(i) <- inner.(i) + inner.(i+1) done
+
+let outer = Array.make rows 0
+let () = outer.(rows-1) <- inner.(rows-1) + Random.int 3
+let () = for i = rows - 2 downto 0 do
+    outer.(i) <- Random.int 3 + max inner.(i) outer.(i+1) done
+
+let n = ref 0
+let () = Array.iteri (fun i out -> n := !n + out - inner.(i)) outer
+let eval = random_partition !n
+
+let () =
+  printf "./lrrule64.opt ";
+  for i = 0 to rows-1 do printf "%d " outer.(i) done; printf "- ";
+  for i = 0 to rows-1 do printf "%d " inner.(i) done; printf "- ";
+  Array.iter (fun p -> printf "%d " p) eval; printf "0@."
+
+
