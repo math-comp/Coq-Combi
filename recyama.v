b3d777d3a4a1b9bef703a776a1f18e5e1ef0c36d
Florent Hivert <Florent.Hivert@lri.fr>
2015-07-13 15:12:59 +0200

    Cleanup in recyama

--- recyama.v
+++ recyama.v
@@ -7,54 +7,36 @@ Require Import tools combclass partition yama stdtab.
 Set Implicit Arguments.
 Unset Strict Implicit.
 
-Section BLa.
+Section RecYama.
 
-  Definition decr_nth_part (p : intpart) i : intpart :=
-    if Sumbool.sumbool_of_bool (is_out_corner p i) is left pf then
-      IntPart (is_part_decr_nth (intpartP p) pf)
+  Definition decr_nth_part_def (p : seq nat) i : (seq nat) :=
+    if is_out_corner p i then decr_nth p i
     else p.
 
-(*
-  Definition decr_nth_part (p : intpart) i : intpart :=
-    if (boolP (is_out_corner p i)) is AltTrue pf then
-      IntPart (is_part_decr_nth (intpartP p) pf)
-    else p.
+  Lemma is_part_decr_nth_part (p : intpart) i :
+    is_part (decr_nth_part_def p i).
+  Proof.
+    rewrite /decr_nth_part_def.
+    case: (boolP (is_out_corner p i)).
+    - apply is_part_decr_nth; exact: intpartP.
+    - move=> _; exact: intpartP.
+  Qed.
 
-  Lemma bla (A B : Type) (f : A -> B) (C : Prop) b1 b2 (X : alt_spec C b1 b2) ptrue pfalse:
-    f (match X with
-         | AltTrue pf => ptrue pf
-         | AltFalse pf => pfalse pf
-       end) =
-    match X with
-      | AltTrue pf => f (ptrue pf)
-      | AltFalse pf => f (pfalse pf)
-    end.
-  Proof. by case: X. Qed.
-*)
-
-
-  Lemma bla (A B : Type) (f : A -> B) (C D : Prop) (X : sumbool C D) ptrue pfalse:
-    f (match X with
-         | left pf => ptrue pf
-         | right pf => pfalse pf
-       end) =
-    match X with
-      | left pf => f (ptrue pf)
-      | right pf => f (pfalse pf)
-    end.
-  Proof. by case: X. Qed.
+  Definition decr_nth_part (p : intpart) i  : intpart :=
+    IntPart (is_part_decr_nth_part p i).
 
   Lemma decr_nth_partE (p : intpart) i :
     is_out_corner p i -> decr_nth_part p i = decr_nth p i :> seq nat.
   Proof.
-    rewrite /decr_nth_part bla /=.
+    rewrite /decr_nth_part /decr_nth_part_def /=.
     by case: (is_out_corner p i).
   Qed.
 
   Lemma card_yama_rec (p : intpart) :
     (p != [::] :> seq nat) ->
     #|yameval_finType p| =
-    sumn [seq #|yameval_finType (decr_nth_part p i)|  | i <- out_corners p ].
+    \sum_(i <- out_corners p)
+      #|yameval_finType (decr_nth_part p i)|.
   Proof.
     move=> H.
     rewrite cardE enumT unlock /= -(size_map val) subType_seqP.
@@ -63,13 +45,13 @@ Section BLa.
     case: n Hn => [| n'] Hn' /=.
       exfalso; move: H.
       by rewrite (part0 (intpartP p) Hn').
-    rewrite size_flatten /shape.
-    rewrite /out_corners -map_comp.
+    rewrite size_flatten /shape sumn_map_condE.
+    rewrite /out_corners -!map_comp.
     congr sumn; apply eq_in_map => i /=.
     rewrite mem_filter => /andP [] Hcorn _.
     rewrite size_map.
     rewrite cardE enumT unlock /= -(size_map val) subType_seqP /=.
-    rewrite /enum_yameval (decr_nth_partE Hcorn).
+    rewrite /enum_yameval /= /decr_nth_part_def /= Hcorn.
     by rewrite (sumn_decr_nth (intpartP p) Hcorn) Hn' /=.
   Qed.
 
@@ -80,10 +62,18 @@ Section BLa.
   Qed.
 
   Lemma card_yam_stdtabE (p : intpart) :
-    #|yameval_finType p| = #|stdtabsh_finType (intpartP p)|.
+    #|yameval_finType p| = #|stdtabsh_finType p|.
   Proof.
-    admit.
+    rewrite /stdtabsh_finType /= !cardE !enumT /= unlock /= -(size_map val) subType_seqP.
+    rewrite size_pmap_sub.
+    suff : all (is_stdtab_of_shape p) (enum_stdtabsh p).
+      by rewrite all_count => /eqP ->; rewrite size_map.
+    rewrite /enum_stdtabsh; apply/allP => t /mapP [] y Hy -> {t}.
+    have /allP Hall := enum_yamevalP (intpartP p).
+    have {Hy Hall} := Hall _ Hy.
+    rewrite /is_yam_of_eval /is_stdtab_of_shape /= => /andP [] /stdtab_of_yamP -> /=.
+    move=> /eqP <-.
+    by rewrite shape_stdtab_of_yam.
   Qed.
 
-
-  
+End RecYama.
