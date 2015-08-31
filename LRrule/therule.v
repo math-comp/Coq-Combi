b792787f536240c519466c8a462dc233629e714d
Florent Hivert <Florent.Hivert@lri.fr>
2015-08-31 21:50:42 +0200

    Renamed multpoly.v => Schur.v

--- LRrule/therule.v
+++ LRrule/therule.v
@@ -17,7 +17,7 @@ Require Import tuple finfun finset bigop path.
 
 Require Import tools ordcast combclass partition Yamanouchi ordtype std stdtab.
 Require Import Schensted congr plactic Greene_inv stdplact Yam_plact skewtab.
-Require Import shuffle multpoly.
+Require Import shuffle Schur.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
