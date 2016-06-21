e31fe4f3adb103fe263c3b619e2bfe6245f317db
Thibaut BENJAMIN <thibaut.benjamin@polytechnique.edu>
2016-06-21 11:10:34 +0200

    update of rsk

--- src/rsk.v
+++ src/rsk.v
@@ -1,7 +1,7 @@
 Require Import mathcomp.ssreflect.ssreflect.
 From mathcomp Require Import ssreflect ssrbool ssrfun ssrnat eqtype finfun fintype choice seq tuple path.
 From mathcomp Require Import finset perm fingroup matrix ssralg.
-Require Import tools combclass subseq partition Yamanouchi permuted ordtype Schensted plactic Greene_inv std.
+Require Import tools combclass subseq partition Yamanouchi permuted ordtype Schensted plactic Greene_inv std stdtab.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -172,17 +172,14 @@ Fixpoint mat_of_bimon (w:seq ('I_m*'I_n)) : 'M_(m,n):=
   (*Par récurrence sur le nombre de lignes => utiliser row' ou [u|d]submx ?
     Dans tous les cas, il faut définir ce qu'est une matrice à laquelle on ajoute une ligne, col_mx doit pouvoir marcher la dessus
 *)
-
-Check row (Ordinal (ltnSn m)).
-
+(*
 Definition col_mx_ind (A:'M[T]_(m.+1,n)) := let M':= row' (Ordinal (ltnSn m)) A in let r := row (Ordinal (ltnSn m)) A in A = col_mx M' r.
-
+*)
 Lemma bimon_lex (M:'M_(m,n)): sorted (lexico2 (@leqm m)  (@leqm n)) (bimon_of_mat M).
 Proof.
   elim: m M => [|m0 IHm0 M0].
   - admit.
   - 
-
 Admitted.
 
 Lemma matK (M:'M_(m,n)): mat_of_bimon (bimon_of_mat M) = M.
@@ -192,8 +189,34 @@ Admitted.
 Lemma bimonK (w: seq ('I_m*'I_n)) : sorted (lexico2 (@leqm m)  (@leqm n)) w -> bimon_of_mat (mat_of_bimon w) = w.
 Proof.
 Admitted.
-  
+
+Definition bottomw_of_mat (M:'M_(m,n)): seq 'I_n :=
+  [seq (snd p)| p <- (bimon_of_mat M)].
+
+Definition upw_of_mat (M:'M_(m,n)): seq 'I_m :=
+  [seq (fst p)| p <- (bimon_of_mat M)].
+
+(*Definition bla: seq nat :=
+  [seq (x:nat)| x <- enum 'I_m].
+*)
 End bimon_mat.
-  
+
+Section Schensted.
+
+Variables m n : nat.
+
+Fixpoint tab_of_yam y w :=
+  if y is y0 :: y' then
+    if w is w0 :: w' then 
+    append_nth (stdtab_of_yam y') w0 y0
+    else [::]
+  else [::].
+
+
+Definition RSKmap (M : 'M[nat]_(m.+1, n.+1)) :=
+  let (P,Q) := RSmap (bottomw_of_mat M) in
+  (P, tab_of_yam Q (upw_of_mat M)).
+
+End Schensted.
 
 (* - utiliser la correspondance RSK (cf) Schensted.v en remplacant les 1,2,3... qui apparaissent dans Q, par 1,1,1,2,2,3,3,... et montrer que c'est un tableau*)
