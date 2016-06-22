6c67f06ccb6021d74e32f47135c18d18badcf4a5
Florent Hivert <Florent.Hivert@lri.fr>
2016-06-22 16:57:13 +0200

    Better specif using ordtypes

--- src/rsk.v
+++ src/rsk.v
@@ -11,132 +11,6 @@ Local Open Scope ring_scope.
 Import GRing.Theory.
 Open Local Scope ring_scope.
 
-Section Hello.
-Variable m n: nat.
-
-Definition bimon_of_mat (M : 'M[nat]_(m, n)) : seq ('I_m * 'I_n) :=
-  flatten [seq nseq (M a b) (a,b) | a <- enum 'I_m, b <- enum 'I_n].
-
-Definition M := \matrix_(i<2,j<3) 1%N.
-
-End Hello.
-
-Check @bimon_of_mat 2 3.
-Check M (Ordinal (lt0n 2)) (Ordinal (lt0n 3)).
-Check bimon_of_mat (m:=2) (n:=3) M.
-(*Eval compute in ((bimon_of_mat M == [:: (0,0); (0,1); (0,2); (1,0); (1,1); (1,2)])%N).*)
-
-
-Section Lexico2.
-
-Variable T R: eqType.
-Variable r: rel T.
-Variable r': rel R.
-Variable order: [/\ reflexive r, antisymmetric r & transitive r].
-Variable order': [/\ reflexive r', antisymmetric r' & transitive r'].
-
-
-Definition lexico2 : rel (T*R) :=
-  fun (p1: T*R) => fun(p2: T*R) =>
-  match p1 with
-  |pair i j => match p2 with
-               |pair k l => (r i k && (i!=k))||((i==k) && r' j l)
-               end
-  end.
-
-Lemma lexico2_refl: reflexive lexico2.
-Proof.
-  case => a b /=.
-  apply /orP; right.
-  rewrite eqxx andTb.
-  by apply order'.
-Qed.
-
-Lemma lexico2_antisym: antisymmetric lexico2.
-Proof.
-  case => a b; case => c d => /= /andP [].
-  have anti: antisymmetric r by apply order.
-  have anti': antisymmetric r' by apply order'.
-  move => /orP; case => /andP [H1 H2];
-  move => /orP; case => /andP [].
-  - move => H3.
-    contradict H2; apply /negP; rewrite negbK; apply /eqP.
-    by apply anti; apply /andP.
-  - rewrite eq_sym => H3.
-    by contradict H2; apply /negP; rewrite negbK.
-  - rewrite eq_sym => _ H3.
-    by contradict H3; apply /negP; rewrite negbK.
-  - move => _ H3.
-    move: H1 => /eqP ->. 
-    rewrite (_: b=d) //.
-    by apply anti'; apply /andP.
-Qed.
-
-
-Lemma lexico2_trans: transitive lexico2.
-Proof.
-  case => a b; case => c d; case => e f /=.
-  have trans': transitive r' by apply order'.
-  have trans: transitive r by apply order.
-  have anti: antisymmetric r by apply order.
-  move => /orP [] /andP [H1 H2]; 
-  move => /orP [] /andP [H3 H4];
-  apply /orP.                    
-  - left.
-    have Hr: r c e by apply (trans a c e).
-    rewrite Hr andTb.
-    apply /negP => /eqP Heq; subst c.
-    move: H4; apply /negP; rewrite negbK; apply /eqP.
-    by apply anti; apply /andP.
-  - left.
-    apply /andP.
-    by move: H3 => /eqP H; subst a.
-  - left.
-    apply /andP.
-    by move: H1 => /eqP H; subst c.
-  - right.
-    apply /andP; split.
-    + by move: H1 => /eqP ->.
-    + by apply (trans' b d f).
-Qed.
-
-Lemma order_lexico2: [/\ reflexive lexico2, antisymmetric lexico2 & transitive lexico2].
-Proof.
-  split.
-  - exact: lexico2_refl.
-  - exact: lexico2_antisym.
-  - exact: lexico2_trans.
-Qed.
-
-End Lexico2.
-
-Section Lexico.
-
-
-Variable T: eqType.
-Variable r: rel T.
-Variable order: [/\ reflexive r, antisymmetric r & transitive r].
-
-Definition lexico : rel (T*T) := lexico2 r r. 
-
-Lemma lexico_refl: reflexive lexico.
-Proof. by apply lexico2_refl. Qed.
-
-Lemma lexico_antisym: antisymmetric lexico.
-Proof. by apply lexico2_antisym. Qed.
-
-
-Lemma lexico_trans: transitive lexico.
-Proof. by apply lexico2_trans. Qed.
-
-Lemma order_lexico: [/\ reflexive lexico, antisymmetric lexico & transitive lexico].
-Proof.
-  split.
-  - exact: lexico_refl.
-  - exact: lexico_antisym.
-  - exact: lexico_trans.
-Qed.
-End Lexico.
 
 Section add_mx.
 (*Not in MathComp (as far as I know) but useful here : 
@@ -149,25 +23,30 @@ Definition add_mx m n (A:'M_(m,n)) k i0 j0 : 'M_(m,n):=
 End add_mx.
 
 
-Section leq_m.
-  
-Definition leqm m: rel ('I_m) :=
-  fun x => fun y => x <= y.
-  
-End leq_m.
-
 Section bimon_mat.
-Variables m n: nat.
-Variable T: eqType.
 
-Fixpoint mat_of_bimon (w:seq ('I_m*'I_n)) : 'M_(m,n):=
+Variables m' n': nat.
+Notation m := m'.+1.
+Notation n := n'.+1.
+Definition alph := ('I_m * 'I_n)%type.
+
+Canonical alph_pordType := Eval hnf in POrdType alph (prodlex_pordMixin _ _).
+
+Definition alph_ordMixin :=
+  Order.Mixin (T := alph_pordType) (@prodlex_total _ _).
+Canonical alph_ordType :=
+  Eval hnf in OrdType alph alph_ordMixin.
+
+Definition bimon_of_mat (M : 'M[nat]_(m, n)) : seq alph :=
+  flatten [seq nseq (M a b) (a,b) | a <- enum 'I_m, b <- enum 'I_n].
+
+Fixpoint mat_of_bimon (w : seq alph) : 'M_(m,n):=
   match w with
-  |[::] => \matrix_(i, j) (0%N)
-  |(p::b) => match p with |pair i0 j0 =>
-                           let M := mat_of_bimon b in add_mx M 1 i0 j0
-                          
-             end
+  | [::] => \matrix_(i, j) (0%N)
+  | ((i0, j0) :: b) => let M := mat_of_bimon b in add_mx M 1 i0 j0
   end.
+
+End bimon_mat.
 
   (*Par récurrence sur le nombre de lignes => utiliser row' ou [u|d]submx ?
     Dans tous les cas, il faut définir ce qu'est une matrice à laquelle on ajoute une ligne, col_mx doit pouvoir marcher la dessus
@@ -175,45 +54,63 @@ Fixpoint mat_of_bimon (w:seq ('I_m*'I_n)) : 'M_(m,n):=
 (*
 Definition col_mx_ind (A:'M[T]_(m.+1,n)) := let M':= row' (Ordinal (ltnSn m)) A in let r := row (Ordinal (ltnSn m)) A in A = col_mx M' r.
 *)
-Lemma bimon_lex (M:'M_(m,n)): sorted (lexico2 (@leqm m)  (@leqm n)) (bimon_of_mat M).
+
+Section Bla.
+
+Variables m' n': nat.
+Notation m := m'.+1.
+Notation n := n'.+1.
+Notation alph := (alph m' n').
+
+Definition col_mx_ind (A:'M[nat]_(m'.+1, n'.+1)) :=
+  let M':= row' (Ordinal (ltnSn m')) A in
+  let r := row (Ordinal (ltnSn m')) A in A = col_mx M' r.
+
+
+Lemma bimon_lex (M : 'M_(m,n)): is_row (bimon_of_mat M).
 Proof.
+  
+  (*
   elim: m M => [|m0 IHm0 M0].
   - admit.
-  - 
+  - *)
 Admitted.
 
 Lemma matK (M:'M_(m,n)): mat_of_bimon (bimon_of_mat M) = M.
 Proof.
 Admitted.
   
-Lemma bimonK (w: seq ('I_m*'I_n)) : sorted (lexico2 (@leqm m)  (@leqm n)) w -> bimon_of_mat (mat_of_bimon w) = w.
+Lemma bimonK (w: seq alph) : is_row w -> bimon_of_mat (mat_of_bimon w) = w.
 Proof.
 Admitted.
 
 Definition bottomw_of_mat (M:'M_(m,n)): seq 'I_n :=
-  [seq (snd p)| p <- (bimon_of_mat M)].
+  [seq p.2 | p <- (bimon_of_mat M)].
 
 Definition upw_of_mat (M:'M_(m,n)): seq 'I_m :=
-  [seq (fst p)| p <- (bimon_of_mat M)].
+  [seq p.1 | p <- (bimon_of_mat M)].
 
-End bimon_mat.
+End Bla.
 
 Section Schensted.
 
-Variables m n : nat.
+Variables m' n' : nat.
+Notation m := m'.+1.
+Notation n := n'.+1.
+Notation alph := (alph m' n').
 
-Fixpoint tab_of_yam y (w: seq 'I_m.+1) :=
+Fixpoint tab_of_yam_rev y (w: seq 'I_m) : seq (seq 'I_m) :=
   if y is y0 :: y' then
-    if w is w0 :: w' then 
-    append_nth (tab_of_yam y' w') w0 y0
+    if w is w0 :: w' then
+    append_nth (tab_of_yam_rev y' w') w0 y0
     else [::]
   else [::].
 
+Definition tab_of_yam y w := tab_of_yam_rev y (rev w).
 
-Definition RSKmap (M : 'M[nat]_(m.+1, n.+1)) :=
+Definition RSKmap (M : 'M[nat]_(m, n)) : seq (seq 'I_n) * seq (seq 'I_m):=
   let (P,Q) := RSmap (bottomw_of_mat M) in
   (P, tab_of_yam Q (upw_of_mat M)).
-
 
 (*Definition istabpair (pair : seq(seq 'I_n) * seq (seq 'I_m)) :=
   let: (P,Q) := pair in
