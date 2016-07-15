29857a04cdaf99f50b0a5bbef6d701d6089b6872
Florent Hivert <Florent.Hivert@lri.fr>
2016-07-15 10:52:23 +0200

    WIP

--- src/reprS3.v
+++ src/reprS3.v
@@ -26,31 +26,31 @@ Section StdRepr.
   
 Notation natS3 := (nat_repr 3).
   
-Definition trivline : 'M[algC]_(3,3) :=
- \matrix_(i < 3,j < 3) (if i == ord0 then 1 else 0).
+Definition trivline : 'M[algC]_(1,3) :=
+ \matrix_(i < 1,j < 3) 1.
 
-Lemma trivlineE : mxmodule natS3 trivline. 
+Lemma trivlineE : mxmodule natS3 trivline.
 Proof.
   rewrite /mxmodule.
   apply /subsetP => /= s _.
   rewrite !inE andTb.
-  have -> // : trivline *m natS3 s = trivline. 
+  have -> // : trivline *m natS3 s = trivline.
   apply /matrixP => i j.
   rewrite /mulmx !mxE.
-  rewrite /natS3 /= /nat_mx /perm_mx /row_perm /scalar_mx.
-  rewrite (eq_bigr (fun j0 => (if i == ord0 then 1 else 0) * ((s j0)==j)%:R));
-    last by move => j0 _; rewrite !mxE.
-  case (boolP (i == ord0)) => [|/negbTE] ->.
-  - rewrite (eq_bigr (fun i0 => (s i0 == j)%:R)); last by move => i0 _; rewrite mul1r.
-    rewrite (bigD1 (s^-1 j)%g) //=.
-    rewrite permKV eqxx /=.
-    rewrite big1 ?addr0 //= => i0 i0jneq.    
-    rewrite (_: (_==_) = false) //=.
-    apply /negP => /eqP i0jeq.
-    by move: i0jneq; rewrite -i0jeq permK eqxx.
-  - by rewrite big1 // => i0 _; rewrite mul0r.
+  rewrite (eq_bigr (fun i0 => (s i0 == j)%:R));
+    last by move => j0 _; rewrite !mxE mul1r.
+  rewrite (bigD1 (s^-1 j)%g) //=.
+  rewrite permKV eqxx /=.
+  rewrite big1 ?addr0 //= => i0 i0jneq.
+  rewrite (_: (_==_) = false) //=.
+  apply /negP => /eqP i0jeq.
+  by move: i0jneq; rewrite -i0jeq permK eqxx.
 Qed.
 
+addsmx_module
+  
+Definition stdS3 := val_factmod 
+                      
 Lemma stdP : mxsplits natS3 1%:M trivline.
 Proof.
   apply mx_Maschke => /=.
