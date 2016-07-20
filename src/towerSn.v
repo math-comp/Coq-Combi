b4cf6b03f88c5d3f21633e128da9423fff94d9d1
Thibaut BENJAMIN <thibaut.benjamin@gmail.com>
2016-07-20 15:21:58 +0200

    added proofs

--- src/towerSn.v
+++ src/towerSn.v
@@ -27,39 +27,24 @@ Variables (G : {group gT}) (H : {group aT}).
 
 
 Lemma cfExtProd_subproof (f1 : 'CF(G)) (f2 : 'CF(H)) :
-  is_class_fun <<[set (x, y) | x in G, y in H]>>
+  is_class_fun <<setX G H>>
                [ffun x : (gT * aT) => ((f1 x.1) * (f2 x.2))%R].
 Proof.
-  apply intro_class_fun => [x y|].
   rewrite genGid.
-Admitted.
+  apply intro_class_fun => [x y|].
+  - rewrite !inE => /andP [x1 x2] /andP [y1 y2].
+  by rewrite !cfunJgen ?genGid.
+  - move => x; rewrite inE => /nandP [x1|x2].
+    + by rewrite cfun0gen ?mul0r ?genGid.
+    + by rewrite [f2 _]cfun0gen ?mulr0 ?genGid.
+Qed.
+
 
 Definition cfExtProd f1 f2 := Cfun 0 (cfExtProd_subproof f1 f2).
-
-Variables (m n : nat).
-
-Local Notation i1 := (@pairg1 [finGroupType of 'S_m] [finGroupType of 'S_n]).
-Local Notation i2 := (@pair1g [finGroupType of 'S_m] [finGroupType of 'S_n]).
-
-
-Lemma cfExtProd_subproof (f1 : 'CF([set: 'S_m])) (f2 : 'CF([set: 'S_n])) :
-  is_class_fun <<[set: 'S_m * 'S_n]>>
-               [ffun x : ('S_m * 'S_n) => ((f1 x.1) * (f2 x.2))%R].
-Proof.
-  admit.
-Admitted.
 
 (*
-Definition cfExtProd (f1 : 'CF([set: 'S_m])) (f2 : 'CF([set: 'S_n])):=
-  [ffun x : ('S_m * 'S_n) => ((f1 x.1) * (f2 x.2))%R].
-Lemma cfExtProd_subproof f1 f2 :
-  is_class_fun <<[set: 'S_m*'S_n]>> (cfExtProd f1 f2).
-Proof.
-  admit.
-Admitted.
-*)
-    
-Definition cfExtProd f1 f2 := Cfun 0 (cfExtProd_subproof f1 f2).
+Local Notation i1 := (@pairg1 gT aT).
+Local Notation i2 := (@pair1g gT aT).
 
 Lemma cfExtProdl f1 f2 : (cfExtProd f1 f2) \o i1 =1 f1.
 Proof.
@@ -71,61 +56,61 @@ Lemma cfExtProdr f1 f2 : (cfExtProd f1 f2) \o i2 =1 f2.
 Proof.
   admit.
 Admitted.
+*)
 
 End cfExtProd.
 
+
 Section ProdRepr.
-(*
-Variables (gT rT : finGroupType).
-Variables (G : {group gT}) (H : {group rT}).
-Variables (n1 n2 : nat) (rG : mx_representation algCF G n1) (rH : mx_representation algCF H n2).
-Lemma extprod_mx_repr : mx_repr (G*H) (fun g => tprod (rG g.1) (rH g.2)).
-*)
 
-Variables (m n : nat).
-Variables (n1 n2 : nat). 
-Variables (rSm : mx_representation algCF [set: 'S_m] n1)
-          (rSn : mx_representation algCF [set: 'S_n] n2).
+Variables (gT aT : finGroupType).
+Variables (G : {group gT}) (H : {group aT}).
+Variables (n1 n2 : nat).
+Variables (rG : mx_representation algCF G n1)
+          (rH : mx_representation algCF H n2).
 
-  
-Lemma extprod_mx_repr : mx_repr [set: 'S_m * 'S_n]
-                                (fun g => tprod (rSm g.1) (rSn g.2)).
+Lemma extprod_mx_repr : mx_repr (setX G H) (fun g => tprod (rG g.1) (rH g.2)).
 Proof.
-  admit.
-Admitted.
+  split=>[|i j]; first by rewrite !repr_mx1 tprod1.
+  rewrite !inE => /andP [i1 i2] /andP [j1 j2].
+  by rewrite !repr_mxM // tprodE.
+Qed.
 
 Definition extprod_repr := MxRepresentation extprod_mx_repr.
-
 End ProdRepr.
+
+Section cfRepr_ExtProd.
+Variables (gT aT : finGroupType).
+Variables (G : {group gT}) (H : {group aT}).
+
+
+Lemma cfRepr_extprod n1 n2
+      (rG : mx_representation algCF G n1)
+      (rH : mx_representation algCF H n2):
+  cfExtProd (cfRepr rG) (cfRepr rH) = cfRepr (extprod_repr rG rH).
+Proof.
+  apply/cfun_inP=> x GXHx.
+  have := GXHx; rewrite !inE => /andP [Gx Hx].
+  by rewrite !cfunE /= Gx Hx GXHx mxtrace_prod.
+Qed.
+
+End cfRepr_ExtProd.
 
 Variables (m n: nat).
 
-
-Lemma cfRepr_prod n1 n2
-      (rSm : mx_representation algCF [set: 'S_m] n1)
-      (rSn : mx_representation algCF [set: 'S_n] n2):
-  cfExtProd (cfRepr rSm) (cfRepr rSn) = cfRepr (extprod_repr rSm rSn).
-Proof.
-  admit.
-Admitted.
-
 Local Notation ct := cycle_typeSN.
-(* Local Notation npart := (intpartn_cast (card_ord n)).
-Local Notation mpart := (intpartn_cast (card_ord m)).
-Local Notation partmn := (intpartn_cast (esym (card_ord (m+n)))).
-*)
 
-Definition pval (s : 'S_m * 'S_n) :=
+Definition tinjval (s : 'S_m * 'S_n) :=
   fun (x : 'I_(m+n)) => let y := split x in
   match y with
   |inl a => unsplit (inl (s.1 a))
   |inr a => unsplit (inr (s.2 a))                  
   end.
 
-Lemma pval_inj s: injective (pval s).
+Lemma tinjval_inj s: injective (tinjval s).
 Proof.
   move=> x y.
-  rewrite /pval.
+  rewrite /tinjval.
   case: {2 3}(split x) (erefl (split x)) => [] a Ha;
     case: {2 3} (split y) (erefl (split y)) => [] b Hb;
       move=> /(congr1 (@split _ _)); rewrite !unsplitK => [] // [];
@@ -133,28 +118,34 @@ Proof.
       by rewrite -(splitK x) Ha -Hb splitK.
 Qed.
 
-Definition tinj s :'S_(m + n) := perm (@pval_inj s).
+Definition tinj s :'S_(m + n) := perm (@tinjval_inj s).
 
 Lemma tinjM (s1 s2 : 'S_m * 'S_n) : (tinj (s1 * s2)%g) = (tinj s1) * (tinj s2)%g. 
 Proof.
   admit.
 Admitted.
 
-Lemma pmorphM: {in [set: 'S_m*'S_n] &, {morph tinj : x y / x *y >-> x * y}}. 
+Lemma pmorphM:
+  {in (setX [set: 'S_m] [set: 'S_n]) &, {morph tinj : x y / x *y >-> x * y}}. 
 Proof.
-  admit.
-Admitted.
-  
+  move=> /= s1 s2 _ _.
+  rewrite /tinj; apply /permP => /= x.
+  rewrite permM -(splitK x) !permE.  
+  case: splitP => [] j _;
+  by rewrite /tinjval !unsplitK /= permM.
+Qed.
+
 Canonical morph_of_tinj := Morphism pmorphM.
 
 (*the image of 'S_m*'S_n via tinj endowed with a group structure of type 'S_(m+n)*)
 Definition prodIm := tinj @* ('dom tinj).
 
 (*i1 and i2 are canonical injection from S_m and S_n to S_m*S_n*)
-
 Local Notation i1 := (@pairg1 [finGroupType of 'S_m] [finGroupType of 'S_n]).
 Local Notation i2 := (@pair1g [finGroupType of 'S_m] [finGroupType of 'S_n]).
 
+
+(*
 Definition p1 := tinj \o i1.
 
 Lemma p1morphM : {in [set: 'S_m] &, {morph p1 : x y / x *y >-> x * y}}.
@@ -172,12 +163,15 @@ Proof.
 Admitted.
 
 Canonical morh_of_p2 := Morphism p2morphM.
+ *)
+
+
 
 (*injm and injn are the images of 'S_m and 'S_n in S_(m+n) via tinj \o i1 and tinj \o i2*)
-Definition injm := p1@*('dom p1).
+(*Definition injm := p1@*('dom p1).
 Definition injn := p2@*('dom p2).
-
-Lemma isomtinj : isom [set: 'S_m*'S_n] prodIm tinj.
+*)
+Lemma isomtinj : isom (setX [set: 'S_m] [set: 'S_n]) prodIm tinj.
 Proof.
   admit.
 Admitted.
