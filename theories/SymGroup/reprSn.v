208494733517ba68c6cd51a94f3333c5cf6ed4d5
Florent Hivert <Florent.Hivert@lri.fr>
2018-09-12 10:09:59 +0200

    Merged reprdim1.v and reprS2.v into reprSn.v

--- theories/SymGroup/reprSn.v
+++ theories/SymGroup/reprSn.v
@@ -1,4 +1,4 @@
-(** * Combi.SymGroup.reprdim1 : Representation of Dimension 1 of the Symmetric Groups *)
+(** * Combi.SymGroup.reprSn : Basic representations of the Symmetric Groups *)
 (******************************************************************************)
 (*       Copyright (C) 2014 Florent Hivert <florent.hivert@lri.fr>            *)
 (*                                                                            *)
@@ -13,14 +13,37 @@
 (*                                                                            *)
 (*                  http://www.gnu.org/licenses/                              *)
 (******************************************************************************)
+(** * Basic representation of the Symmetric Groups
+
+We define the two representations of dimension 1 together with the natural
+representation, and show that ['S_2] has only 2 representations. More precisely,
+for a fixed [n] and  [g : 'S_n]:
+
+- [triv_mx g] == the 1x1 identity matrix.
+- [sign_mx g] == the 1x1 scalar matrix equal to the sign of [g].
+- [signed_mx R g] == given a representation [R] the matrix [sign g * R g]
+- [nat_mx g] == the permutation matrix associated to [g]
+
+These four matrices are actually representation matrices:
+
+- [triv_repr] == the trivial representation
+- [sign_repr] == the sign representation
+- [signed_repr R] == the representation [R] times the sign
+- [nat_repr n] == the natural representation of degree [n]
+
+We show in Lemmas [repr1] and [repr_S2] that [triv_repr] and [sign_repr]
+exhausts all representations of ['S_n] of degree 1 all irreducible
+representations of of ['S_2].
+*************)
 Require Import mathcomp.ssreflect.ssreflect.
 From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
 From mathcomp Require Import fintype tuple finfun bigop finset fingroup perm.
 From mathcomp Require Import ssralg fingroup morphism perm action.
-From mathcomp Require Import matrix mxalgebra ssrnum algC.
+From mathcomp Require Import zmodp. (* Defines the coercion nat -> 'I_n.+1 *)
+From mathcomp Require Import vector matrix mxalgebra ssrnum algC.
 From mathcomp Require Import mxrepresentation classfun character.
 
-Require Import permcomp tools congr presentSn.
+Require Import permcomp tools sorted partition congr cycles cycletype presentSn.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -32,6 +55,7 @@ Local Open Scope ring_scope.
 Local Notation reprS n d :=
   (mx_representation [fieldType of algC] 'SG_n d).
 
+(** * Representation of dimension 1 and natural representation *)
 Section DefTrivSign.
 
 Variable n d : nat.
@@ -84,12 +108,13 @@ Proof.
 Qed.
 Canonical nat_repr : reprS n n := MxRepresentation nat_mx_repr.
 
-
 End DefTrivSign.
+
 Arguments triv_mx_repr [n].
 Arguments triv_repr [n].
 Arguments sign_mx_repr [n].
 Arguments sign_repr [n].
+
 
 Lemma row_free1 : row_free (1 : 'M[algC]_1).
 Proof. by apply/row_freeP; exists 1; rewrite mul1mx. Qed.
@@ -161,4 +186,84 @@ Proof.
     case: (odd_perm (\prod_(i <- w) eltr n i)) => /=;
       rewrite mulmxE mulN1r //.
     exact: opprK.
+Qed.
+
+
+
+(** * Representations of the symmetric Group for n=2  *)
+
+Lemma NirrSn n : Nirr 'SG_n = #|{:'P_n}|.
+Proof using. by rewrite NirrE card_classes_perm card_ord. Qed.
+
+Lemma NirrS2 : Nirr 'SG_2 = 2.
+Proof using. by rewrite NirrSn card_intpartn. Qed.
+
+Lemma cfRepr_triv n : cfRepr (triv_repr (n := n)) = 'chi_0.
+Proof using.
+rewrite -cfunP irr0 => s.
+rewrite cfunE cfun1E !inE mulr1n.
+rewrite /triv_repr /triv_mx /=.
+by rewrite mxtrace1.
+Qed.
+Lemma triv_Chi n : mx_rsim (triv_repr (n := n)) 'Chi_0.
+Proof using. by apply/cfRepr_rsimP; rewrite cfRepr_triv irrRepr. Qed.
+
+Lemma cast_IirrS2 (i : Iirr 'SG_2) :
+  i != 0 -> i = cast_ord (esym NirrS2) 1.
+Proof using.
+move=> Hi; apply val_inj => /=.
+by case: i Hi => [[|[|i]]] //=; rewrite NirrS2.
+Qed.
+
+Lemma cfRepr_sign2 :
+  cfRepr (sign_repr (n := 2)) = 'chi_(cast_ord (esym NirrS2) 1).
+Proof using.
+have : cfRepr sign_repr \in irr 'SG_2.
+  apply/irr_reprP.
+  by exists (Representation sign_repr); first exact: sign_irr.
+move=> /irrP [j]; rewrite -!irrRepr => /eqP/cfRepr_rsimP/mx_rsim_sym Hj.
+apply/eqP/cfRepr_rsimP.
+apply (mx_rsim_trans (mx_rsim_sym Hj)).
+rewrite (cast_IirrS2 (i := j)); first exact: mx_rsim_refl.
+apply/eqP => Hj0; subst j.
+apply: (triv_sign_not_sim (n := 2)) => //.
+exact: mx_rsim_trans (triv_Chi 2) Hj.
+Qed.
+
+Lemma sign_Chi2 : mx_rsim (sign_repr (n := 2)) 'Chi_(cast_ord (esym NirrS2) 1).
+Proof using. by apply/cfRepr_rsimP; rewrite cfRepr_sign2 irrRepr. Qed.
+
+Lemma char_S2 :
+  irr 'SG_2 = tcast (esym NirrS2) [tuple cfRepr triv_repr; cfRepr sign_repr].
+Proof using.
+apply eq_from_tnth => i.
+case: (altP (i =P 0)) => [-> | Hi].
+- by rewrite tcastE {2}/tnth /= cfRepr_triv; congr 'chi_(_); exact: val_inj.
+- by rewrite tcastE {2}/tnth /= cfRepr_sign2 (cast_IirrS2 Hi) /=.
+Qed.
+
+Lemma perm_eq_char_S2 :
+  perm_eq [:: cfRepr triv_repr; cfRepr sign_repr] (irr 'SG_2).
+Proof using.
+have Huniq : uniq [:: cfRepr (triv_repr (n := 2)); cfRepr sign_repr].
+  rewrite /= andbT inE; apply/cfRepr_rsimP.
+  exact: triv_sign_not_sim.
+apply uniq_perm_eq => //; first by apply free_uniq; exact: irr_free.
+apply leq_size_perm => //.
+move=> i; rewrite !inE => /orP [] /eqP ->; apply/irr_reprP.
+- by exists (Representation triv_repr); first exact: triv_irr.
+- by exists (Representation sign_repr); first exact: sign_irr.
+- have:= NirrSn 2; rewrite card_intpartn /=.
+  have -> : intpartn_nb 2 = 2 by [].
+  by rewrite size_tuple /= => ->.
+Qed.
+
+Lemma repr_S2 (rho : representation [fieldType of algC] [group of 'SG_2]) :
+  mx_irreducible rho -> mx_rsim rho triv_repr \/ mx_rsim rho sign_repr.
+Proof using.
+move=> Hirr.
+have : cfRepr rho \in irr 'SG_2.
+  apply/irr_reprP; by exists rho.
+by rewrite -(perm_eq_mem perm_eq_char_S2) !inE =>
+  /orP [] /cfRepr_rsimP; [left | right].
 Qed.
