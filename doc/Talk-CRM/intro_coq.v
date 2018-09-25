74bf117858837f2cdf7200d0b18a36b33fdc6db2
Florent Hivert <Florent.Hivert@lri.fr>
2018-09-25 06:42:26 -0400

    Darij rereading

--- doc/Talk-CRM/intro_coq.v
+++ doc/Talk-CRM/intro_coq.v
@@ -1,5 +1,6 @@
 Require Import mathcomp.ssreflect.ssreflect.
-From mathcomp Require Import ssrbool ssrfun ssrnat eqtype fintype choice seq.
+From mathcomp Require Import ssrbool ssrfun ssrnat eqtype.
+From mathcomp Require Import  fintype choice seq bigop div.
 
 
 (** Term, judgments and computation *)
@@ -8,10 +9,10 @@ Check true.
 Check (true : bool).
 Check 3.
 Check (3 : nat).
-(** Check (3 : bool). *)
+(* Check (3 : bool). *)
 Check [:: 3; 4].
 Check ([:: 3; 4] : seq nat).
-(** Check ([:: 3; 4; 2] : nat). *)
+(* Check ([:: 3; 4; 2] : nat). *)
 
 Check (fun n => n + 1).
 Check ((fun n => n + 1) 3).
@@ -38,7 +39,7 @@ Definition is_ok ans :=
   | yes => true
   | no => false
   | dontknow => false
-  end.
+   end.
 
 Check is_ok.
 Eval compute in is_ok yes.
@@ -72,6 +73,7 @@ Print eq.
 Print and.
 Print or.
 
+Check erefl.  
 Check (or_introl (erefl 3) : 3 = 3 \/ 3 = 5).
 
 Print False. (* A proposition which has by definition no proof *)
@@ -88,7 +90,7 @@ Fixpoint iterate
   if n is n'.+1 then step (iterate T init step n') else init.
 
 Fixpoint iterate_dep
-         (T : nat -> Type)
+         (T : nat -> Prop)
          (init : T 0)
          (step : forall (n : nat), T n -> T n.+1)
          (n : nat) :=
@@ -149,3 +151,57 @@ Eval compute in has (fun x => x == 5) [:: 2; 5; 3; 4].
 Eval compute in has (fun x => x == 0) [:: 2; 5; 3; 4].
 Check hasP.
 
+
+
+
+
+Fixpoint is_part shape :=
+  if shape is shape0 :: shape'
+  then (shape0 >= head 1 shape') && (is_part shape')
+  else true.
+
+Fixpoint incr_first_n shape n :=
+  if shape is s0 :: s then
+    if n is n'.+1 then s0.+1 :: incr_first_n s n'
+    else shape
+  else nseq n 1.
+Fixpoint conj_part shape :=
+  if shape is s0 :: shape then incr_first_n (conj_part shape) s0
+  else [::].
+
+Lemma is_part_nseq1 n : is_part (nseq n 1).
+Proof.
+by elim: n => [//= | n /= ->]; rewrite andbT; case n.
+Qed.
+
+Lemma is_part_incr_first_n shape n :
+  is_part shape -> is_part (incr_first_n shape n).
+Proof.
+elim: shape n => [// n _| s0 shape IHshape] /=.
+   exact: is_part_nseq1.
+case=> [//= | n] /andP [Hhead /IHshape{IHshape} /= ->].
+ rewrite andbT.
+case: shape Hhead => [_ | s1 shape] /=; first by case n.
+case: n => [| n] /=; last by apply.
+by move/leq_trans; apply.
+Qed.
+
+Lemma is_part_conj shape :
+  is_part shape -> is_part (conj_part shape).
+Proof.
+elim: shape => [//= | s0 shape IHshape] /=.
+move=> /andP [_ /IHshape{IHshape}].
+exact: is_part_incr_first_n.
+Qed.
+
+
+
+Lemma sum n :
+  \sum_(0 <= i < n.+1) i = n * (n + 1) %/ 2.
+Proof.
+elim : n => [//= | n IHn].
+  by rewrite !mul0n div0n big_nat_recl // big_nat big_pred0.
+rewrite big_nat_recr //= {}IHn.
+rewrite [LHS]addnC -divnMDl //; congr (_ %/ _).
+by ring_simplify.
+Qed.
